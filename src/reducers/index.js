13e38cce40192f86ceb89213d34feebaa4d30d22
Janet Karpenske <janetkarpenske@gmail.com>
2020-12-11 11:10:21 -0800

    Add editing, form-visible, and root reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,14 @@
+import formVisibleReducer from './form-visible-reducer';
+import postListReducer from './post-list-reducer';
+import editingReducer from './editing-reducer';
+import postSelectReducer from './post-select-reducer';
+import { combineReducers } from 'redux';
+
+const rootReducer = combineReducers({
+  formVisibleOnPage: formVisibleReducer,
+  masterPostList: postListReducer,
+  editing: editingReducer,
+  selectedPost: postSelectReducer
+});
+
+export default rootReducer;
