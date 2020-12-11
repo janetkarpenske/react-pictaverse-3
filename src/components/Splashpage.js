283eeb42b545d31800e27741e73aff39bcbe44b3
Janet Karpenske <janetkarpenske@gmail.com>
2020-12-11 08:12:00 -0800

    Initial commit on Friday

---
+++ src/components/Splashpage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Splashpage () {
+  return (
+    <React.Fragment>
+      <div className="mainHeader"><hr /></div>
+      <div className="center-align">
+        <h1 className="title">pictaverse</h1>
+        <h3>A photography-driven travel guide.</h3>
+        <h6><strong>Created By: Janet Karpenske</strong></h6>
+      </div>
+      <hr/>
+      
+    </React.Fragment>
+  );
+}
+
+export default Splashpage;
