3d7c5b7c4c644e96c40d26b43844f6155ec4a043
Janet Karpenske <janetkarpenske@gmail.com>
2020-12-11 13:56:41 -0800

    Add styling to homepage

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import Header from "./Header";
+import Homepage from "./Homepage";
 import PostControl from "./PostControl"; 
 
 function App(){
   return ( 
     <React.Fragment>
-      <Header />
+      <Homepage />
       <div className="container-fluid">
         <PostControl />
       </div>
---
+++ src/components/Homepage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Homepage () {
+  return (
+    <React.Fragment>
+        <div className="mainHeader heroText">
+          <div className="center-align">
+            <h1 className="title">picta<span className="green">v</span>erse</h1>
+            <h3 className="subHead">A photography-driven travel guide.</h3>
+            <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+            <h6>Janet M .Karpenske</h6>
+          </div>
+        </div>
+    </React.Fragment>
+  );
+}
+
+export default Homepage;
