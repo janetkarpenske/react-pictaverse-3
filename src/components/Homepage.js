d18fd35339efa9ae361daadd548d53d9d815f04e
Janet Karpenske <janetkarpenske@gmail.com>
2020-12-14 17:27:23 -0800

    Like and dislike in place. Background photo changed.

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -6,7 +6,7 @@ function Homepage () {
         <div className="mainHeader heroText">
           <div className="center-align">
             <h1 className="title">picta<span className="green">v</span>erse</h1>
-            <h3 className="subHead">A photography-driven travel guide.</h3>
+            <h3 className="subHead">The photography-driven travel guide.</h3>
             <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
             <h6>Janet M .Karpenske</h6>
           </div>
