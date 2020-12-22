4b6edd51ce7fb86be0dc6f08298f862c29b099cc
Janet Karpenske <janetkarpenske@gmail.com>
2020-12-22 13:05:42 -0800

    Still working on user authentication

--- src/components/signIn.js
+++ src/components/signIn.js
@@ -1,8 +1,36 @@
 import React from "react";
+import firebase from "firebase/app";
 
 function Signin(){  
+
+    function doSignUp(event) {
+      event.preventDefault();
+      const email = event.target.email.value;
+      const password = event.target.password.value;
+      firebase.auth().createUserWithEmailAndPassword(email, password).then(function(){
+        console.log("successfully signed up!");
+      }).catch(function(error) {
+        console.log(error.message);
+      });
+    }
+
   return (
-    <h1>Sign In Page</h1>
+    <React.Fragment>
+      <div className="whiteBackground">
+      <h1>Sign up</h1>
+      <form onSubmit={doSignUp}>
+        <input
+          type='text'
+          name='email'
+          placeholder='email' />
+        <input
+          type='password'
+          name='password'
+          placeholder='Password' />
+        <button type='submit'>Sign up</button>
+      </form>
+      </div>
+    </React.Fragment>
   );
 }
 
