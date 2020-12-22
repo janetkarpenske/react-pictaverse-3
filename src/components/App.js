6900c18abf0e2510d0ee4f157cad13ef432898c9
Janet Karpenske <janetkarpenske@gmail.com>
2020-12-22 08:18:58 -0800

    Change green styling colors

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,26 @@ import React from "react";
 import PostControl from "./PostControl"; 
 import Header from "./Header";
 
+import Signin from "./Signin";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 function App(){
   return ( 
     <React.Fragment>
-      <div className="container-fluid">
-        <Header />
-        <PostControl />
-      </div>
+      <Router>
+        <div className="container-fluid">
+          <Header />
+          <Switch>
+            <Route path="/signin">
+              <Signin />
+            </Route>
+            <Route path="/">
+              <PostControl />
+            </Route>
+          </Switch>
+        </div>
+        
+      </Router>
     </React.Fragment>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Header(){  
   return (
     <React.Fragment>
     <ul className="headerList">
-      <li>home</li>
-      <li>sign in & register</li>
+      <li><Link to="/">home</Link></li>
+      <li><Link to="/signin">sign in & register</Link></li>
       <li className="nameItem">picta<span className="green">v</span>erse</li>
     </ul>
     </React.Fragment>
